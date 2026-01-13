813ce2219eb014ee1b68de316e3445b4d846e338
Oesch.me <54373321+OeschMe@users.noreply.github.com>
2026-01-13 11:23:14 +0200

    Update paper.sh
    Update API URL

--- paper.sh
+++ paper.sh
@@ -12,7 +12,7 @@ MC_VERSION="${MC_VERSION,,}"
 PAPER_BUILD="${PAPER_BUILD,,}"
 
 # Get version information and build download URL and jar name
-URL='https://api.papermc.io/v3/projects/paper'
+URL='https://fill.papermc.io/v3/projects/paper'
 if [[ $MC_VERSION == latest ]]
 then
   # Get the latest MC version
